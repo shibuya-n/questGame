c5074c52e765b1e05ad32282093647c717798473
shibuya-n <a.duong2006@gmail.com>
2023-10-03 15:08:38 -0700

    test

--- src/Games.java
+++ src/Games.java
@@ -12,6 +12,34 @@ public class Games {
 
     public void scramble(){
         System.out.println("Welcome to the scramble quest, challenger " + name);
+
+        String x = "";
+        String solutions = "";
+
+        for (int i = 0; i < 6; i++) {
+
+            int ascii = (int)(Math.random() * 26) + 97;
+            x = x + " " + (char)ascii;
+
+        }
+        System.out.println("Unscramble this string: ");
+        System.out.println(x);
+
+        for (int i = 0; i < name.length(); i++){
+
+
+            if (x.length() == 1) {
+                solution += temp;
+                break;
+            }
+            else {
+                scramble += temp.charAt(random);
+
+                temp = temp.substring(0, random) + temp.substring(random + 1);
+            }
+
+        }
+    }
     }public void math(){
         if (this.level <=2) {
             System.out.println("You aren't strong enough for this level yet. :(");
@@ -34,9 +62,6 @@ public class Games {
 
         }else {
             //add challenge (binary convert?)
-            //test
-            //douvle test
-            //test
         }
     }
 }
--- src/Quest.java
+++ src/Quest.java
@@ -3,43 +3,52 @@ import java.util.*;
 public class Quest {
 
     public static void main(String[] args) {
-        ask(getInput());
-    }
-
-    public static void ask(String x){
         System.out.println("What is your name, traveler?");
         Games obj = new Games(getInput());
-        String input = x;
 
-        System.out.println("Welcome to League of Letters, " + obj.name);
+
+        ask(obj);
+    }
+
+    public static void ask(Games y){
+
+
+
+
+
+        System.out.println("Welcome to League of Letters, " + y.name);
         System.out.println("Which game would you like to play?");
         System.out.println("1. Scramble");
         System.out.println("2. Math");
         System.out.println("3. Trivia");
         System.out.println("FINAL BOSS (unlocked after level 3)");
 
+        String input = getInput();
+
         // add animation?
         // dynamic selection?
 
         while (true){
-            if (input.equals("1")){
-                obj.scramble();
-            }
-            else if (input.equals("2")){
-                obj.math();
-            }
-            else if (input.equals("3")){
-                obj.trivia();
-            }
-            else if (input.equals("4")){
-                obj.challenge();
-            }
-            else {
-                ask(getInput());
-            }
-            if (obj.win){
+            if (y.win){
                 break;
             }
+
+            if (input.equals("1")){
+                y.scramble();
+            }
+            else if (input.equals("2")){
+                y.math();
+            }
+            else if (input.equals("3")){
+                y.trivia();
+            }
+            else if (input.equals("4")){
+                y.challenge();
+            }
+            else {
+                ask(y);
+            }
+
         }
 
     }
